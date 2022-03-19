fbadde818d27e7177b008e7ca0df235ca0495675
kaifeng-yu16 <90477174+kaifeng-yu16@users.noreply.github.com>
2022-03-19 05:50:06 -0400

    Create download_model.py

---
+++ download_model.py
@@ -0,0 +1,9 @@
+from transformers import AutoTokenizer, AutoModelForCausalLM
+
+tokenizer = AutoTokenizer.from_pretrained("gpt2")
+model = AutoModelForCausalLM.from_pretrained("gpt2")
+
+from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
+
+tokenizer = AutoTokenizer.from_pretrained("Helsinki-NLP/opus-mt-zh-en")
+model = AutoModelForSeq2SeqLM.from_pretrained("Helsinki-NLP/opus-mt-zh-en")
